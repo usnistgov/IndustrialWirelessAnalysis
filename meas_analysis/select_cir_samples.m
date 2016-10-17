ad2af725920e364aff6c42e831dda4369e7d0d3a
rcandell <rick.candell@nist.gov>
2016-10-17 12:46:36 -0400

    threshold for CIR selection adjusted from 15 to 20 dB from peak

--- meas_analysis/select_cir_samples.m
+++ meas_analysis/select_cir_samples.m
@@ -31,10 +31,13 @@ pk_pwr = 10*log10(abs(max(cir).^2));
 
 % linear domain thresholds
 nft = 10;           % 10 dB above noise floor
-pkt = 1/31.6228;    % 15 dB from peak
+pkt = 1/1000.0;    % 10-20 dB from peak is ITU recommendation
+% the dynamic range of our instrumentation allows for a larger calculation
+% domain but we select 30 dB here but the delay spread calculation we
+% further refine to 15 dB
 
-% eliminate the anomalous tail components (last 8 samples)
-cir = cir(1:round(length(cir)*0.5));
+% eliminate the anomalous tail components 
+cir = cir(1:round(length(cir)*0.8));
 
 % compute the magnitude squared of the cir normailzed to the peak value
 cir_mag2 = (abs(cir).^2);
@@ -42,12 +45,14 @@ cir_max = max(cir_mag2);
 cir_mag2 = (abs(cir).^2)/cir_max;
 
 % select the cir sample for use in average cir estimation
-nf = mean(cir_mag2(round(length(cir)*0.8):round(length(cir)*0.9)));
+nf = mean(cir_mag2(round(length(cir)*0.8):round(length(cir)*1.0)));
 k = find( (cir_mag2 > nf*nft) & (cir_mag2 > pkt) );
 
 % modified cir
 cir1 = zeros(size(cir));
 cir1(k) = cir(k);
+
+%plot([10*log10(cir_mag2) 10*log10(abs(cir1/max(abs(cir1))).^2)],'+-'),xlim([0 500]),refline(0,10*log10(nf)+10),refline(0,-30)
 
 end
 
